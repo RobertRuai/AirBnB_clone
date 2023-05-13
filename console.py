82528d28296a7a4524f422c6987206e0446fbce8
Robert Ruai Mawich <robertruai4@gmail.com>
2023-05-13 18:41:34 +0300

    imported other classes to conosole

--- console.py
+++ console.py
@@ -2,6 +2,12 @@
 import cmd
 from models.base_model import BaseModel
 from models import FileStorage
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):

8339e7f1072731565e1c3c50a9104f629210bac5
Rayoo <105439282+ra-y-oo@users.noreply.github.com>
2023-05-14 15:34:59 +0300

    Update test_console.py

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,7 +1,7 @@
 import unittest
 from unittest.mock import patch
 from io import StringIO
-from console import HBNBCommand
+from models.console import HBNBCommand
 from models import storage
 from models.base_model import BaseModel
 from models.user import User
